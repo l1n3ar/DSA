17f5be16b26f7fc598f02a4a2eb81786db5a69a0
Mehul Chattopadhyay <66662596+mehul2015@users.noreply.github.com>
2022-04-04 23:23:48 +0530

    Added solution - LeetHub

--- 5. Fibonacci Using Recursion - GFG/5.-fibonacci-using-recursion.java
+++ 5. Fibonacci Using Recursion - GFG/5.-fibonacci-using-recursion.java
@@ -28,8 +28,14 @@ class Solution
 {
     static int fibonacci(int n)
     {
-        if(n == 0 || n == 1) return n;
-        return fibonacci(n-1) + fibonacci(n-2);
+        int[] arr = new int[n+1];
+        arr[0] = 0;
+        arr[1] = 1;
+        for(int i = 2;i<arr.length;i++){
+            arr[i] = arr[i-1] + arr[i-2];
+        }
+        
+        return arr[n];
     }
 }
 
