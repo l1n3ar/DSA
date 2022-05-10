f46d78a848d82f0c0edc1094f9fef6dd4f685f34
Mehul Chattopadhyay <66662596+mehul2015@users.noreply.github.com>
2022-05-10 21:17:24 +0530

    Time: 68 ms (27.06%), Space: 118.1 MB (31.56%) - LeetHub

---
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -0,0 +1,68 @@
+class Solution {
+    
+    class Point implements Comparable<Point> {
+        
+        int[]coordinates = new int[2];
+        int distance;
+        
+        Point(int[] coordinates, int distance){
+            this.coordinates = coordinates;
+            this.distance = distance;
+        }
+        
+        public int compareTo(Point p){
+            return this.distance - p.distance;
+        }
+        
+    }
+    public int[][] kClosest(int[][] points, int k) {
+        
+        int[][] result = new int[k][2];
+        if(points.length == 0 || k > points.length) return result;
+        
+        PriorityQueue<Point> pq = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for(int i = 0;i<k;i++){
+            
+            int[] currentCoordinates = new int[2];
+            currentCoordinates[0] = points[i][0];
+            currentCoordinates[1] = points[i][1];
+            int currentDistance = distance(points[i][0],points[i][1]);
+            
+            pq.add(new Point(currentCoordinates, currentDistance));
+        }
+        
+        for(int i = k;i<points.length;i++){
+            
+            int[] currentCoordinates = new int[2];
+            currentCoordinates[0] = points[i][0];
+            currentCoordinates[1] = points[i][1];
+            int currentDistance = distance(points[i][0],points[i][1]);
+            
+            int distanceFromQueue = pq.peek().distance;
+            
+            if(currentDistance < distanceFromQueue){
+                pq.poll();
+                pq.add(new Point(currentCoordinates, currentDistance));
+            }
+            
+    
+        }
+        
+        int index = 0;
+        while(!pq.isEmpty()){
+            int[] coordinates = pq.poll().coordinates;
+            result[index][0] = coordinates[0];
+            result[index][1] = coordinates[1];
+            index++;
+        }
+        
+        return result;
+        
+    }
+    
+    public int distance(int x, int y){
+        int squaredDistance = (int) (Math.pow(x,2) + Math.pow(y,2));
+        return (int) squaredDistance;
+    }
+}
