afe298a0e6fe497068ad9ebe24720472cc151d8c
Mehul Chattopadhyay <66662596+mehul2015@users.noreply.github.com>
2022-05-16 21:58:13 +0530

    Update 55-jump-game.java

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -1,3 +1,5 @@
+//Greedy Solution
+
 class Solution {
     public boolean canJump(int[] nums) {
         
@@ -14,5 +16,32 @@ class Solution {
         
         
         return goal == 0;
+    }
+}
+
+//DP Solution
+
+class Solution {
+    public boolean canJump(int[] nums) {
+        
+        int n = nums.length;
+        boolean[] can = new boolean[n];
+        
+        can[n-1] = true;
+        
+        for(int i = n-2;i>=0;i--){
+            int maxJumpFromCurrentIndex = nums[i];
+            int currentIndex = i;
+            
+            if(currentIndex + maxJumpFromCurrentIndex >= n-1) can[currentIndex] = true;
+            else{
+                for(int j = currentIndex + 1;j<=currentIndex + maxJumpFromCurrentIndex;j++){
+                    if(can[j]) can[currentIndex] = true;
+                }
+            }
+        }
+        
+        return can[0];
+        
     }
 }
