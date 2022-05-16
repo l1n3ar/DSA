83983ceb3d231c69e03761bbf87384ee6ca325c6
Mehul Chattopadhyay <66662596+mehul2015@users.noreply.github.com>
2022-05-16 20:35:33 +0530

    Time: 201 ms (21.53%), Space: 67.3 MB (63.86%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -3,23 +3,30 @@ class Solution {
         
         if(nums.length == 0) return true;
         
-        boolean[] can = new boolean[nums.length];
-        can[nums.length - 1] = true;
+        int n = nums.length;
         
-        for(int i = nums.length-2;i>=0;i--){
+        boolean[] can = new boolean[n];
+        can[n-1] = true;
+        
+        for(int i = n-2;i>=0;i--){
+            
             int maxJumpFromCurrentIndex = nums[i];
             int currentIndex = i;
-            if(nums[i] == 0) can[currentIndex] = false;
-            else if(maxJumpFromCurrentIndex + currentIndex >= nums.length - 1) can[currentIndex] = true;
+            
+            if(maxJumpFromCurrentIndex == 0) can[currentIndex] = false;
+            else if(maxJumpFromCurrentIndex + currentIndex >= n-1) can[currentIndex] = true;
             else{
                 for(int j = 1;j<=maxJumpFromCurrentIndex;j++){
                     if(can[j+currentIndex]) can[currentIndex] = true;
                 }
             }
-            }
-        
-             return can[0];
+            
         }
         
-   
+        
+        
+        return can[0];
+        
+        
     }
+}
