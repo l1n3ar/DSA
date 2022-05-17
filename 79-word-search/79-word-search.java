1f1512c7d1b1e3a9c2093dac6951207bef0b80e3
Mehul Chattopadhyay <66662596+mehul2015@users.noreply.github.com>
2022-05-17 15:41:12 +0530

    Time: 151 ms (53.39%), Space: 42.1 MB (55.49%) - LeetHub

--- 79-word-search/79-word-search.java
+++ 79-word-search/79-word-search.java
@@ -1,33 +1,42 @@
 class Solution {
+           
+        static boolean found;
     public boolean exist(char[][] board, String word) {
         
-        // boolean found = false;
+        found = false;
         boolean[][] visited = new boolean[board.length][board[0].length];
       
             for(int i = 0;i<board.length;i++){
             for(int j = 0;j<board[0].length;j++){
-                    if(find(i,j,board,word,visited,0)) return true;
+                    find(i,j,board,word,visited,0);
+                    if(found) return true;
                 }
         }
          return false;
     }
     
-    public static boolean find(int x, int y, char[][] arr, String word, boolean[][] visited,int current){
+    public static void find(int x, int y, char[][] arr, String word, boolean[][] visited,int current){
         
-          if(current == word.length()) return true;
+          if(current == word.length()){
+              found = true;
+              return;
+          }
           
         
-        if(x < 0 || y < 0 || x >= arr.length || y >= arr[0].length || visited[x][y]) return false;
-        if(arr[x][y]!=word.charAt(current)) return false;
+        if(x < 0 || y < 0 || x >= arr.length || y >= arr[0].length || visited[x][y]) return;
+        if(arr[x][y]!=word.charAt(current)) return;
         
         visited[x][y] = true;
        
-        boolean result = find(x+1,y,arr,word,visited,current+1)||find(x-1,y,arr,word,visited,current+1)||find(x,y+1,arr,word,visited,current+1)||find(x,y-1,arr,word,visited,current+1);
+        find(x+1,y,arr,word,visited,current+1);
+        find(x-1,y,arr,word,visited,current+1);
+        find(x,y+1,arr,word,visited,current+1);
+        find(x,y-1,arr,word,visited,current+1);
         
         visited[x][y] = false;
         
         
-        return result;
+   
        
     }
 }
