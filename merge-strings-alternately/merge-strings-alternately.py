39f3ebe3cde522154e77df8b2affb2340300bb44
Mehul Chattopadhyay <66662596+mehul2015@users.noreply.github.com>
2023-07-10 12:07:27 +0530

    Time: 43 ms (81.59%) | Memory: 16.2 MB (76.37%) - LeetSync

--- merge-strings-alternately/merge-strings-alternately.py
+++ merge-strings-alternately/merge-strings-alternately.py
@@ -4,28 +4,9 @@ class Solution:
         if word1 is None or word1 == "" : return word2
         if word2 is None or word2 == "" : return word1
 
-        iw1 = 0
-        iw2 = 0
-        l1 = len(word1)
-        l2 = len(word2)
-
         res = ""
 
-        while (iw1 < l1 and iw2 < l2):
+        for i in range(min(len(word1),len(word2))):
+           res+= word1[i] + word2[i]
 
-            res+=word1[iw1]
-            res+=word2[iw2]
-            iw1+=1
-            iw2+=1
-        
-        while (iw1 < l1):
-            res+=word1[iw1]
-            iw1+=1
-
-        
-        while (iw2 < l2):
-            res+=word2[iw2]
-            iw2+=1
-
-        
-        return res
+        return res + word1[i+1:] + word2[i+1:]
