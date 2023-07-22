d31dd4cddcda265a4fd0f3252f14d98addfe97ea
Mehul Chattopadhyay <66662596+mehul2015@users.noreply.github.com>
2023-07-22 10:39:23 +0530

    Time: 354 ms (5.06%) | Memory: 19.1 MB (5.02%) - LeetSync

---
+++ 801-is-graph-bipartite/is-graph-bipartite.py
@@ -0,0 +1,43 @@
+from collections import deque
+from typing import List
+
+class Pair:
+    def __init__(self, vertex, level):
+        self.vertex = vertex
+        self.level = level
+
+class Solution:
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+        q = deque()
+        visited = [0] * len(graph)
+        
+        def findOddCycle(graph, src):
+           
+
+            q.append(Pair(vertex=src, level=1))
+
+            while q:
+                print(q[0].vertex,q[0].level)
+
+                vertex = q.popleft()
+
+
+
+                if visited[vertex.vertex] != 0 and visited[vertex.vertex] != vertex.level:
+                    return True
+                else:
+                    visited[vertex.vertex] = vertex.level
+
+                for nbr in graph[vertex.vertex]:
+                    if visited[nbr]==0 : q.append(Pair(vertex=nbr, level=vertex.level + 1))
+
+            return False
+
+        for i in range(len(graph)):
+            if visited[i] == 0:
+                oddCycle = findOddCycle(graph, i)
+                print(oddCycle)
+                if oddCycle:
+                    return False
+
+        return True
