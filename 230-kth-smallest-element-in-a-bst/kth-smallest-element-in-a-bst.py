7882556ffa663c3ed0c94e4f78110c8ce236a1ce
Mehul Chattopadhyay <66662596+mehul2015@users.noreply.github.com>
2023-07-22 13:56:23 +0530

    Time: 67 ms (59.87%) | Memory: 20.4 MB (50.02%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
@@ -1,38 +1,18 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-
-        if not root : return 0
-        count = 0
-        kth = 0
-
         def dfs(node):
-            nonlocal count
-            nonlocal kth
-
-
-            if not node : return
-
-            if node.left : dfs(node.left)
+            nonlocal k
+            if not node:
+                return None
             
-            count+=1
-            if count == k :
-                kth = node.val
-                return 
-            if node.right : dfs(node.right)
-
+            left_result = dfs(node.left)
+            if left_result is not None:
+                return left_result
+            
+            k -= 1
+            if k == 0:
+                return node.val
+            
+            return dfs(node.right)
         
-        
-        dfs(root)
-        return kth
-
-
-
-        # inorder traversal
-        # start counting at the left most node
-        # when k value is the number of calls return the node
+        return dfs(root)
